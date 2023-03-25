c24cd5d38a6bfcb806bbf578865a61ec8df26fa4
ViniciusGR797 <loritoamarelo@outlook.com>
2023-03-25 18:20:37 -0300

    🎯 Core application

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+
+# Ler o arquivo Excel
+df = pd.read_excel('nutrition.xlsx')
+
+# Converter os dados para um dicionário
+data = df.to_dict(orient='records')
+
+# Converter o dicionário para um objeto JSON
+json_data = json.dumps(data)
+
+# Salvar o JSON em um arquivo
+with open('nutrition.json', 'w') as f:
+    f.write(json_data)
