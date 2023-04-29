473bf75f278de0533b104cef636fda8e4a1e4cac
ViniciusGR797 <loritoamarelo@outlook.com>
2023-04-29 18:18:17 -0300

    🔠 Accentuation in Json

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ try:
     # Converter os dados para um dicionário
     data = df.to_dict(orient='records')
 
-    # Converter o dicionário para um objeto JSON
-    json_data = json.dumps(data)
+    # Converter o dicionário para um objeto JSON, mantendo a acentuação
+    json_data = json.dumps(data, ensure_ascii=False)
 
     # Salvar o JSON em um arquivo
-    with open(json_filename, 'w') as f:
+    with open(json_filename, 'w', encoding='utf-8') as f:
         f.write(json_data)
         print(f'File "{json_filename}" saved successfully.')
 except Exception as e:
